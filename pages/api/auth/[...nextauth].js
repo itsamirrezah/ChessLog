b09cdd1ae2d63146fb3e710da9762fc48711be6d
Amirreza <itsamirrezah@gmail.com>
2022-03-28 01:41:25 +0430

    update nextauth session object

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,36 +1,51 @@
 import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
-import { findUserByEmailAndPassword } from "../../../api-lib/database/_users";
+import {
+  getUserByEmailAndPassword,
+  getUserById,
+} from "../../../api-lib/database/users";
 import { connectDatabase } from "../../../api-lib/database/utils";
 
-export default NextAuth({
-  callbacks: {
-    async jwt({ token, user, profile, account, isNewUser }) {
-      if (user) {
-        token.user = user;
-      }
-      return token;
-    },
-    async session({ user, session, token }) {
-      session.user = token.user;
-      return session;
-    },
-  },
-  providers: [
-    Credentials({
-      async authorize(credentials, req) {
-        const { email, password } = credentials;
-        const mongoClient = await connectDatabase();
-        const user = await findUserByEmailAndPassword(
-          mongoClient,
-          email,
-          password
-        );
-        if (!user) {
-          throw new Error("email or password is wrong.");
+export default function handler(req, res) {
+  return NextAuth(req, res, {
+    callbacks: {
+      async jwt({ token, user, profile, account, isNewUser }) {
+        let newUser = user;
+        if (newUser) {
+          token.user = user;
         }
-        return { id: user._id.toString(), email: user.email };
+        if (req.query.update) {
+          const client = await connectDatabase();
+          newUser = await getUserById(client, token.user.id);
+          token.user = {
+            id: newUser._id,
+            email: newUser.email,
+            username: newUser.username,
+            name: newUser.name,
+          };
+        }
+        return token;
       },
-    }),
-  ],
-});
+      async session({ user, session, token }) {
+        session.user = token.user;
+        return session;
+      },
+    },
+    providers: [
+      Credentials({
+        async authorize(credentials, req) {
+          const { email, password } = credentials;
+          const client = await connectDatabase();
+          const user = await getUserByEmailAndPassword(client, email, password);
+
+          return {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+            name: user.name,
+          };
+        },
+      }),
+    ],
+  });
+}
